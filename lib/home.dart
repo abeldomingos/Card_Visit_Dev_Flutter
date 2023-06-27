85b74f40c7cc04c53a6315845733fc92b9df48da
Abel Kamulingui <70151385+abeldomingos@users.noreply.github.com>
2023-06-27 18:32:14 +0200

    commit degrade aolicado

--- lib/home.dart
+++ lib/home.dart
@@ -8,91 +8,104 @@ class Home extends StatelessWidget {
     return  Scaffold(
       backgroundColor: Colors.blue.shade600,
       body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:  [
-               const CircleAvatar(
-                backgroundImage: AssetImage('imagens/photo.png',),
-                radius: 60,
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                  colors: [
+                    Colors.blue,
+                    Colors.grey,
+                    //Colors.orange.shade400,
+                  ]
+                  tileMode: 
               ),
-              const Text('Abel Kamulingui',
-                style:  TextStyle(
-                  fontSize: 25,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w400,
-                  fontFamily: 'Lobster',
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:  [
+                const CircleAvatar(
+                  backgroundImage: AssetImage('imagens/photo.png',),
+                  radius: 60,
                 ),
-              ),
+                const Text('Abel Kamulingui',
+                  style:  TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400,
+                    fontFamily: 'Lobster',
+                  ),
+                ),
 
-              const Text('DESENVOLVIDOR FLUTTER',
-              style: TextStyle(
-                fontSize: 12,
-                color: Colors.white,
-              ),
-              ),
+                const Text('DESENVOLVIDOR FLUTTER',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.white,
+                  ),
+                ),
 
-              const SizedBox(
-                height: 18,
-                width: 250,
-                child:  Divider(
-                  color: Colors.white30,
+                const SizedBox(
+                  height: 18,
+                  width: 250,
+                  child:  Divider(
+                    color: Colors.white30,
+                  ),
                 ),
-              ),
 
-              //PHONE
-              Container(
-                margin: const EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 25,
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade700,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                padding: const EdgeInsets.all(12),
-                child: Row(
-                  children: const [
-                     Icon(Icons.phone,
-                      size: 20,
-                      color: Colors.white,
-                    ),
-                     SizedBox(width: 15,),
-                     Text('+244 927 038 823',
-                      style: TextStyle(
+                //PHONE
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 25,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade700,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.all(12),
+                  child: Row(
+                    children: const [
+                      Icon(Icons.phone,
+                        size: 20,
                         color: Colors.white,
                       ),
-                    ),
-                  ],
+                      SizedBox(width: 15,),
+                      Text('+244 927 038 823',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
 
-              //EMAIL
-              Container(
-                margin: const EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 25,
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade700,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                padding: const EdgeInsets.all(12),
-                child: Row(
-                  children: const [
-                    Icon(Icons.email,
-                      size: 20,
-                      color: Colors.white,
-                    ),
-                    SizedBox(width: 15,),
-                    Text('changebleassing@gmail.com',
-                      style:  TextStyle(
+                //EMAIL
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 25,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade700,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.all(12),
+                  child: Row(
+                    children: const [
+                      Icon(Icons.email,
+                        size: 20,
                         color: Colors.white,
                       ),
-                    ),
-                  ],
+                      SizedBox(width: 15,),
+                      Text('changebleassing@gmail.com',
+                        style:  TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
       ),
     );
